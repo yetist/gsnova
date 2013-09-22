c4eab8754f9e6ab9d44175a9f059b98b8bdb88cb
yetist <yetist@gmail.com>
2013-09-22 16:49:31 +0800

    merge

--- common/path.go
+++ common/path.go
@@ -36,13 +36,13 @@ func init() {
 
 func (c *PathConfig) Init() {
 	//cwd, _ := filepath.Abs(path.Dir(os.Args[0]))
-	cwd := path.Dir(os.Args[0])
+	//cwd := path.Dir(os.Args[0])
 	//PathCfg.Data_dir = path.Join(cwd, "share", "gsnova")
-	PathCfg.Data_dir = path.Join(cwd, "share")
-	PathCfg.User_dir = path.Join(cwd, "user")
-	//PathCfg.User_dir = UserHomeDir()
+	PathCfg.Data_dir = path.Join("/usr", "share", "gsnova")
+	//PathCfg.User_dir = path.Join(cwd, "user")
+	PathCfg.User_dir = UserHomeDir()
 	//PathCfg.conf_path = path.Join(cwd, "data", "gsnova.conf")
-	PathCfg.conf_path = path.Join(cwd, "share", "gsnova.conf")
+	PathCfg.conf_path = path.Join("/etc", "gsnova.conf")
 }
 
 func UserHomeDir() string {
